183dee0f308c8d677001f80dfac5ab2843267f80
Jared Chavez <jared@undeadlabs.net>
2019-02-02 18:37:03 -0800

    add support for c files

--- main.go
+++ main.go
@@ -1,147 +1,153 @@
 package main
 
 import (
-    "bufio"
-    "bytes"
-    "flag"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "os"
-    "regexp"
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
 )
 
 // regexp
 var (
-    regBranchId = []*regexp.Regexp {
-        // cs, go, py
-        regexp.MustCompile("(.*[bB]ranch_?[iI]d\\s*=\\s*)(.\\d*)(.*)"),
-        // json
-        regexp.MustCompile("(.*\"[bB]ranch_?[iI]d\"\\s*:\\s*)(.\\d*)(.*)"),
-    }
-    regBranchName = []*regexp.Regexp {
-        // cs, go, py
-        regexp.MustCompile("(.*[bB]ranch_?[nN]ame\\s*=\\s*[\"'])(.*)([\"'].*)"),
-        // json
-        regexp.MustCompile("(.*\"[bB]ranch_?[nN]ame\"\\s*:\\s*\")(.*)(\".*)"),
-    }
-    regBuildId = []*regexp.Regexp {
-        // cs, go, py
-        regexp.MustCompile("(.*[bB]uild_?[iI]d\\s*=\\s*)(\\d*)(.*)"),
-        // json
-        regexp.MustCompile("(.*\"[bB]uild_?[iI]d\"\\s*:\\s*)(\\d*)(.*)"),
-    }
-    regBuildConfig = []*regexp.Regexp {
-        // cs, go, py
-        regexp.MustCompile("(.*[bB]uild_?[cC]onfig\\s*=\\s*[\"'])(.*)([\"'].*)"),
-        // json
-        regexp.MustCompile("(.*\"[bB]uild_?[cC]onfig\"\\s*:\\s*\")(.*)(\".*)"),
-    }
+	regBranchId = []*regexp.Regexp{
+		// cs, go, py
+		regexp.MustCompile("(.*[bB]ranch_?[iI]d\\s*=\\s*)(.\\d*)(.*)"),
+		// json
+		regexp.MustCompile("(.*\"[bB]ranch_?[iI]d\"\\s*:\\s*)(.\\d*)(.*)"),
+		// c
+		regexp.MustCompile("(#define BRANCH_ID\\s*)(.\\d*)(.*)"),
+	}
+	regBranchName = []*regexp.Regexp{
+		// cs, go, py
+		regexp.MustCompile("(.*[bB]ranch_?[nN]ame\\s*=\\s*[\"'])(.*)([\"'].*)"),
+		// json
+		regexp.MustCompile("(.*\"[bB]ranch_?[nN]ame\"\\s*:\\s*\")(.*)(\".*)"),
+	}
+	regBuildId = []*regexp.Regexp{
+		// cs, go, py
+		regexp.MustCompile("(.*[bB]uild_?[iI]d\\s*=\\s*)(\\d*)(.*)"),
+		// json
+		regexp.MustCompile("(.*\"[bB]uild_?[iI]d\"\\s*:\\s*)(\\d*)(.*)"),
+		// c
+		regexp.MustCompile("(#define BUILD_ID\\s*)(\\d*)(.*)"),
+	}
+	regBuildConfig = []*regexp.Regexp{
+		// cs, go, py
+		regexp.MustCompile("(.*[bB]uild_?[cC]onfig\\s*=\\s*[\"'])(.*)([\"'].*)"),
+		// json
+		regexp.MustCompile("(.*\"[bB]uild_?[cC]onfig\"\\s*:\\s*\")(.*)(\".*)"),
+		// c
+		regexp.MustCompile("(#define BUILD_CONFIG\\s*\")(.*)(\".*)"),
+	}
 )
 
 // config vars
 var (
-    path        string
-    branch      string
-    branchId    int
-    buildConfig string
-    buildId     int
+	path        string
+	branch      string
+	branchId    int
+	buildConfig string
+	buildId     int
 )
 
 func main() {
-    parseArgs()
-    labelBuild()
+	parseArgs()
+	labelBuild()
 }
 
 func labelBuild() {
-    var buffer bytes.Buffer
+	var buffer bytes.Buffer
 
-    f, err := os.Open(path)
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-    reader := bufio.NewReader(f)
-    for {
-        line, err := reader.ReadString('\n')
-        if err == io.EOF {
-            break
-        }
+	reader := bufio.NewReader(f)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
 
-        if err != nil {
-            panic(err)
-        }
+		if err != nil {
+			panic(err)
+		}
 
-        replaced, newLine := replaceInfo(
-            regBranchId,
-            line,
-            fmt.Sprintf("${1}%d${3}", branchId),
-        )
-        if replaced {
-            buffer.WriteString(newLine)
-            continue
-        }
+		replaced, newLine := replaceInfo(
+			regBranchId,
+			line,
+			fmt.Sprintf("${1}%d${3}", branchId),
+		)
+		if replaced {
+			buffer.WriteString(newLine)
+			continue
+		}
 
-        replaced, newLine = replaceInfo(
-            regBranchName,
-            line,
-            fmt.Sprintf("${1}%s${3}", branch),
-        )
-        if replaced {
-            buffer.WriteString(newLine)
-            continue
-        }
+		replaced, newLine = replaceInfo(
+			regBranchName,
+			line,
+			fmt.Sprintf("${1}%s${3}", branch),
+		)
+		if replaced {
+			buffer.WriteString(newLine)
+			continue
+		}
 
-        replaced, newLine = replaceInfo(
-            regBuildConfig,
-            line,
-            fmt.Sprintf("${1}%s${3}", buildConfig),
-        )
-        if replaced {
-            buffer.WriteString(newLine)
-            continue
-        }
+		replaced, newLine = replaceInfo(
+			regBuildConfig,
+			line,
+			fmt.Sprintf("${1}%s${3}", buildConfig),
+		)
+		if replaced {
+			buffer.WriteString(newLine)
+			continue
+		}
 
-        replaced, newLine = replaceInfo(
-            regBuildId,
-            line,
-            fmt.Sprintf("${1}%d${3}", buildId),
-        )
-        if replaced {
-            buffer.WriteString(newLine)
-            continue
-        }
+		replaced, newLine = replaceInfo(
+			regBuildId,
+			line,
+			fmt.Sprintf("${1}%d${3}", buildId),
+		)
+		if replaced {
+			buffer.WriteString(newLine)
+			continue
+		}
 
-        // write line as-is
-        buffer.WriteString(line)
-    }
+		// write line as-is
+		buffer.WriteString(line)
+	}
 
-    f.Close()
+	f.Close()
 
-    ioutil.WriteFile(path, buffer.Bytes(), 0660)
+	ioutil.WriteFile(path, buffer.Bytes(), 0660)
 }
 
 func replaceInfo(regexList []*regexp.Regexp, line, repl string) (bool, string) {
-    for _, regex := range regexList {
-        if len(regex.FindStringSubmatch(line)) == 4 {
-            return true, regex.ReplaceAllString(line, repl)
-        }
-    }
+	for _, regex := range regexList {
+		if len(regex.FindStringSubmatch(line)) == 4 {
+			return true, regex.ReplaceAllString(line, repl)
+		}
+	}
 
-    return false, ""
+	return false, ""
 }
 
 func parseArgs() {
-    flag.StringVar(&path, "path", "", "(required) Path to the source file containing build information")
-    flag.StringVar(&branch, "branchName", "", "(required) The branch to label the source file with")
-    flag.IntVar(&branchId, "branchId", -1, "(required) The numeric branch id to label the source file with")
-    flag.StringVar(&buildConfig, "buildConfig", "Debug", "The build configuration to label the source file with")
-    flag.IntVar(&buildId, "buildId", 0, "The BuildID to label the source file with")
-    flag.Parse()
+	flag.StringVar(&path, "path", "", "(required) Path to the source file containing build information")
+	flag.StringVar(&branch, "branchName", "", "(required) The branch to label the source file with")
+	flag.IntVar(&branchId, "branchId", -1, "(required) The numeric branch id to label the source file with")
+	flag.StringVar(&buildConfig, "buildConfig", "Debug", "The build configuration to label the source file with")
+	flag.IntVar(&buildId, "buildId", 0, "The BuildID to label the source file with")
+	flag.Parse()
 
-    if len(path) < 1 || len(branch) < 1 || branchId == -1 {
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
+	if len(path) < 1 || len(branch) < 1 || branchId == -1 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 }
